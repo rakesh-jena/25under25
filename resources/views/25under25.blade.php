949196a0637b73a271a7ac713c93cc1f80769d57
Rakesh Ranjan Jena <rakeshranjanjena7@gmail.com>
2023-01-06 08:06:40 +0530

    First commit

---
+++ resources/views/25under25.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+    <title>@yield('title','25 under 25')</title>
+    <meta content="@yield('meta_description','default description')" name="description">
+    <meta content="@yield('meta_keywords','some default keywords')" name="keywords">
+    @yield('meta')
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset('images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="icon" href="{{ URL::asset('images/favicon/favicon.ico') }}">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    
+    <!-- bootstrap CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!--aos -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="{{ URL::asset('css/tut.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header">
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-lg bg-white navbarpadding">
+            <div class="container">
+                <a class="navbar-brand nav-logo-dark" href="{{url('/')}}">
+                    <img src="{{ URL::asset('images/25U25-B1.png') }}" alt="25 Under 25" width="150" height="40">
+                </a>
+                <a class="navbar-brand nav-logo-light" href="{{url('/')}}">
+                    <img src="{{ URL::asset('images/25U25-B1.png') }}" alt="25 Under 25" width="150" height="40">
+                </a>
+            </div>
+        </nav>
+    </header>
+    <!-- End Header -->
+
+    <!-- Main Content Wrapper -->
+    <div id="page" class="pt-4 pb-4">
+        @yield('content')
+    </div>
+    <!-- End Main Content Wrapper -->
+    
+    <!-- Footer start-->
+    <!-- Footer End-->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+
+    <!-- Jquery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-migrate-3.4.0.min.js"
+        integrity="sha256-mBCu5+bVfYzOqpYyK4jm30ZxAZRomuErKEFJFIyrwvM=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+    <!-- AOS -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 1200,
+        });
+    </script>
+
+    <!-- Custom JS -->
+    <script src="{{ URL::asset('js/tut.js') }}"></script>    
+</body>
+
+</html>
---
+++ resources/views/tut/homepage.blade.php
@@ -0,0 +1,118 @@
+@extends('25under25')
+@section('title', '25 UNDER 25')
+@section('meta_keywords', 'YIF')
+@section('meta_description', 'News on Indian Youth and Politics')
+
+@section('content')
+<section class="section tut-banner">
+    <div class="">
+        <img src="{{URL::asset('images/25under25/poster_2.jpg')}}" alt="banner">
+    </div>
+</section>
+<section class="section tut-heading">
+    <div class="container">
+        <div class="af-title">
+            <h1>
+                FORBES INDIA 30 UNDER 30 2022: MEET INDIA'S YOUNG DISRUPTORS AND GAMECHANGERS
+            </h1>
+        </div>
+        <div class="af-content">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="{{URL::asset('images/25under25/30-under-20-img.png')}}" alt="25">
+                </div>
+                <div class="col-md-9">
+                    <p class="desc">Disrupting an industry is not easy. Neither is making a name for yourself in a dynamic economy like India's. But achieving all that at a young age<span style="font-family:Arial !important;">—</span>under 30<span style="font-family:Arial !important;">—</span>is no mean feat. Our Class of 2022's entrepreneurs, professionals, designers, influencers, and sportspersons, represent the many Indias we live in, and epitomise that success can come from anyone<span style="font-family:Arial !important;">—</span>and anywhere<span style="font-family:Arial !important;">—</span>as long as they are empowered to shine</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section tut-art-wrap">
+    <div class="container">
+        <div class="art-box">
+            <div class="row">
+                <div class="col-md-8 col-12">
+                    <div class="art-img">
+                        <a href="#">
+                            <img src="{{URL::asset('images/25under25/Neetu-Kirti.jpg')}}" alt="Neetu Kirti">
+                            <span class="art-ttl">
+                                Neetu Yadav, Kirti Jangra, 
+                                <em>Animall</em>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-12">
+                    <div class="art-desc">
+                        <div class="f-ttl">
+                            BY THE NUMBERS
+                        </div>
+                        <div class="ttl-wrap">
+                            <div class="y-ttl">
+                                <span>₹2,800 crore+</span>
+                            </div>
+                            <div class="th-ttl">
+                                FUNDS RAISED BY THE CLASS OF 2022
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="art-list">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <a href="#">
+                        <img src="{{URL::asset('images/25under25/V-Gokul.jpg')}}" alt="Rahul Jain">
+                        <span class="ttl-name">
+                            Rahul Jain, 
+                            <em class="comp-name">
+                                Sideways Consulting
+                            </em>
+                        </span>
+                    </a>
+                </div>
+                <div class="col-md-3 col-6">
+                    <a href="#">
+                        <img src="{{URL::asset('images/25under25/V-Gokul.jpg')}}" alt="Rahul Jain">
+                        <span class="ttl-name">
+                            Rahul Jain, 
+                            <em class="comp-name">
+                                Sideways Consulting
+                            </em>
+                        </span>
+                    </a>
+                </div>
+                <div class="col-md-3 col-6">
+                    <a href="#">
+                        <img src="{{URL::asset('images/25under25/V-Gokul.jpg')}}" alt="Rahul Jain">
+                        <span class="ttl-name">
+                            Rahul Jain, 
+                            <em class="comp-name">
+                                Sideways Consulting
+                            </em>
+                        </span>
+                    </a>
+                </div>
+                <div class="col-md-3 col-6">
+                    <a href="#">
+                        <img src="{{URL::asset('images/25under25/V-Gokul.jpg')}}" alt="Rahul Jain">
+                        <span class="ttl-name">
+                            Rahul Jain, 
+                            <em class="comp-name">
+                                Sideways Consulting
+                            </em>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section tut-art-wrap">
+    <div class="container">
+
+    </div>
+</section>
+@endsection
